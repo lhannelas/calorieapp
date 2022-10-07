11d26f29d15ae83a549af43e6a54aa30eff06469
lhannelas <lhannelas@gmail.com>
2022-10-07 09:05:01 +0400

    first commit

---
+++ counter/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+
+# D5bx4ETmxOnIC/c/gtCkew==LEHPzr79JVNDMbuu
+# Create your views here.
+def home(request):
+    import json
+    import requests
+    if request.method == 'POST':
+        query = request.POST['query']
+        api_url = 'https://api.api-ninjas.com/v1/nutrition?query='
+        api_request = requests.get(api_url + query, headers={'X-Api-Key': 'D5bx4ETmxOnIC/c/gtCkew==LEHPzr79JVNDMbuu'})
+        try:
+            api = json.loads(api_request.content)
+            print(api_request.content)
+        except Exception as e:
+            api = "oops! There was an error"
+            print(e)
+        return render(request, 'home.html', {'api': api})
+    else:
+        return render(request, 'home.html', {'query': 'Enter a valid query'})
+
+
+
+
